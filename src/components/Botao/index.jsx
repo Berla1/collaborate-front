66e4d1790a954a466eb7bfc34b88993c45be8d6d
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-11-22 17:45:25 -0300

    primeira seção landing page

--- src/components/Botao/index.jsx
+++ src/components/Botao/index.jsx
@@ -1,7 +1,12 @@
-const Botao = ({ text, bgColor, textColor }) => {
-    return (
-        <button style={{ backgroundColor: bgColor, color: textColor }} className="px-4 py-2 rounded-lg border-2 border-[#FF7837]">{text}</button>
-      );
-}
- 
+const Botao = ({ text, bgColor, textColor, width }) => {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor, width: width}}
+      className="px-4 py-2 rounded-lg border-2 border-[#FF7837] hover:transition-all hover:shadow-glow"
+    >
+      {text}
+    </button>
+  );
+};
+
 export default Botao;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,29 @@
+import Botao from "../../components/Botao";
 import Nav from "../../components/Nav";
+import Saly from "../../assets/saly-38.svg";
 
 const Home = () => {
-    return (
-        <Nav/>
-      );
-}
- 
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+      <section className="flex mt-24 items-start h-screen ">
+        <div className="flex flex-col gap-6 mt-14">
+          <h1 className="text-[64px] text-white font-bold leading-tight">
+            Conheça pessoas, desenvolva,{" "}
+            <span className="text-[#FF7837]">colabore</span>
+          </h1>
+          <p className="w-1/2 text-white text-2xl">
+            No Collaborate você contribui para projetos reais nas diferentes
+            áreas da programação.
+          </p>
+          <Botao bgColor={"#FF7837"} text={"Explorar"} textColor={"white"} width={"15%"} />
+        </div>
+        <img src={Saly} alt="" />
+      </section>
+    </>
+  );
+};
+
 export default Home;
