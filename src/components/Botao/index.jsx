4e94fe67d6df1c268b70cb1390d89911f5417fd3
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-11-27 12:32:31 -0300

    landing page responsivo (falta o nav)

--- src/components/Botao/index.jsx
+++ src/components/Botao/index.jsx
@@ -1,8 +1,8 @@
 const Botao = ({ text, bgColor, textColor, width }) => {
   return (
     <button
-      style={{ backgroundColor: bgColor, color: textColor, width: width}}
-      className="px-4 py-2 rounded-lg border-2 border-[#FF7837] hover:transition-all hover:shadow-glow"
+      style={{ backgroundColor: bgColor, color: textColor }}
+      className={`px-4 py-2 rounded-lg border-2 border-[#FF7837] ${width} hover:transition-all hover:shadow-glow`}
     >
       {text}
     </button>
--- src/pages/Home/OCollaborate/index.jsx
+++ src/pages/Home/OCollaborate/index.jsx
@@ -5,15 +5,15 @@ import Chat from "../../../assets/landing/chat-3d.svg"
 
 const OCollaborate = () => {
     return (  
-    <section className="flex flex-col gap-8">
-        <h2 className=" flex items-center justify-center gap-8 text-[64px] text-white font-bold mb-20">
-          O que é o <span className="text-[#FF7837]">Collaborate</span>
+    <section className="flex flex-col justify-center items-center gap-8 mt-56">
+        <h2 className=" text-3xl text-white font-bold mb-10 lg:text-[64px] lg:mb-20">
+          O que é o <span className="text-[#FF7837]">Collaborate?</span>
         </h2>
 
         <div className="flex items-center justify-between">
-            <img className="ml-48" src={Joia} alt="" />
+            <img className="ml-48 hidden lg:block" src={Joia} alt="" />
           
-          <p className="w-1/2 text-3xl text-white font-medium ">
+          <p className="text-xl text-white font-medium lg:w-1/2 lg:text-3xl">
             É uma plataforma que une pessoas com habilidades técnicas distintas
             a fim de colaborar para um projeto que pode ser usado para <span  className="text-[#FF7837]">aprender
             tecnologias</span> ou como <span  className="text-[#FF7837]">portfólio</span>.
@@ -21,14 +21,15 @@ const OCollaborate = () => {
         </div>
 
         <div className="flex w-full gap-40">
-          <p className="text-3xl text-white font-medium w-1/2">
+          <p className="text-xl text-white font-medium lg:w-1/2 lg:text-3xl">
           O <span className="text-[#FF7837]">Collaborate</span> nasceu quando percebemos que contribuir para grandes projetos é essencial para aprender novas tecnologias e conhecer pessoas, mas enfrentávamos o <span className="text-[#FF7837]">medo</span> de não dominar certas habilidades e a <span className="text-[#FF7837]">dificuldade de encontrar pessoas</span> para colaborar.
           </p>
-          <img src={Play} alt="" />
+          <img src={Play} className="hidden lg:block" alt="" />
         </div>
+
         <div className="flex items-center justify-between">
-          <img className="ml-48" src={Chat} alt="" />
-          <p className="w-1/2 text-3xl text-white font-medium ">
+          <img className="ml-48 hidden lg:block" src={Chat} alt="" />
+          <p className="text-xl text-white font-medium lg:w-1/2 lg:text-3xl">
             Aqui você pode aprender junto com outras pessoas enquanto cria <span className="text-[#FF7837]">projetos reais</span>, ou até mesmo tirar aquela ideia do papel! <span className="text-[#FF7837]">E ai ta pronto para colaborar?</span>
           </p>
         </div>
